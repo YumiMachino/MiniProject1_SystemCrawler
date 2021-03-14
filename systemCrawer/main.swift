656a9140529f815e46c109e23fe834cedd3cbbdd
YumiMachino <y.y.1515m@gmail.com>
2021-03-14 09:47:24 -0700

    initial commit

--- systemCrawer/main.swift
+++ systemCrawer/main.swift
@@ -7,5 +7,23 @@
 
 import Foundation
 
-print("Hello, World!")
+var temp = [String]();
+func getFileNames(_ path: String) {
+    let fileManager = FileManager.default
+    
+    // get file names inside the directory
+    let fileNames = try? fileManager.contentsOfDirectory(atPath: path)
+    
+    // base case: if there are no items in the contentsOfDirectory
+    if !fileManager.fileExists(atPath: fileNames?[0] ?? "") {
+        print(temp)
+    } else {
+        if let fileNames = fileNames {
+            temp.append(fileNames[0])
+            getFileNames(fileNames[1])
+        }
+    }
+}
+
+getFileNames(FileManager.default.currentDirectoryPath)
 
